8eb13f7b3988687645fac841775e5362edc8ffc6
Ng Wei Han <weihan774237@gmail.com>
2023-04-26 11:41:45 +0800

    add RPC codes

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository stores a compilation of backend concepts.
--- basic-rpc-server/main.go
+++ basic-rpc-server/main.go
@@ -1,72 +1,114 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
 )
 
 type Item struct {
-	title string
-	body  string
+	Title string
+	Body  string
 }
+
+type API int
 
 var database []Item
 
-func GetByTitle(title string) Item {
+func (a *API) GetDB(empty string, reply *[]Item) error {
+	*reply = database
+	return nil
+}
+
+func (a *API) GetByName(title string, reply *Item) error {
 	var getItem Item
+
 	for _, val := range database {
-		if val.title == title {
+		if val.Title == title {
 			getItem = val
 		}
 	}
-	return getItem
+
+	*reply = getItem
+
+	return nil
 }
 
-func AddItem(title string, body string) Item {
-	newItem := Item{title, body}
-	database = append(database, newItem)
-	return newItem
+func (a *API) AddItem(item Item, reply *Item) error {
+	database = append(database, item)
+	*reply = item
+	return nil
 }
 
-func EditItem(title string, body string) Item {
+func (a *API) EditItem(item Item, reply *Item) error {
 	var changed Item
+
 	for idx, val := range database {
-		if val.title == title {
-			database[idx].body = body
+		if val.Title == item.Title {
+			database[idx] = Item{item.Title, item.Body}
 			changed = database[idx]
 		}
 	}
-	return changed
+
+	*reply = changed
+	return nil
 }
 
-func DeleteItem(item Item) Item {
+func (a *API) DeleteItem(item Item, reply *Item) error {
 	var del Item
+
 	for idx, val := range database {
-		if val.title == item.title && val.body == item.body {
+		if val.Title == item.Title && val.Body == item.Body {
 			database = append(database[:idx], database[idx+1:]...)
 			del = item
+			break
 		}
 	}
-	return del
+
+	*reply = del
+	return nil
 }
 
 func main() {
-	fmt.Println("Initial database: ", database)
-	a := Item{"first", "a test item"}
-	b := Item{"second", "a second item"}
-	c := Item{"third", "a third item"}
+	api := new(API)
+	err := rpc.Register(api)
+	if err != nil {
+		log.Fatal("error registering API", err)
+	}
 
-	AddItem(a.title, a.body)
-	AddItem(b.title, b.body)
-	AddItem(c.title, c.body)
-	fmt.Println("Second database: ", database)
+	rpc.HandleHTTP()
 
-	DeleteItem(b)
-	fmt.Println("Third database: ", database)
+	listener, err := net.Listen("tcp", ":4040")
 
-	EditItem(c.title, "a different third item")
-	fmt.Println("Fourth database: ", database)
+	if err != nil {
+		log.Fatal("Listener error", err)
+	}
+	log.Printf("serving rpc on port %d", 4040)
+	http.Serve(listener, nil)
 
-	fmt.Println("GetByTitle: ", GetByTitle("first"))
-	fmt.Println("GetByTitle: ", GetByTitle("third"))
+	if err != nil {
+		log.Fatal("error serving: ", err)
+	}
+
+	// fmt.Println("initial database: ", database)
+	// a := Item{"first", "a test item"}
+	// b := Item{"second", "a second item"}
+	// c := Item{"third", "a third item"}
+
+	// AddItem(a)
+	// AddItem(b)
+	// AddItem(c)
+	// fmt.Println("second database: ", database)
+
+	// DeleteItem(b)
+	// fmt.Println("third database: ", database)
+
+	// EditItem("third", Item{"fourth", "a new item"})
+	// fmt.Println("fourth database: ", database)
+
+	// x := GetByName("fourth")
+	// y := GetByName("first")
+	// fmt.Println(x, y)
 
 }
